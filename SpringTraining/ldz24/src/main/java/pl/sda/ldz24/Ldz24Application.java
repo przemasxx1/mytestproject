7dfbc00cb1be735634052621efd8007be4c7faf6
Przemo <wxm@example.com>
2020-07-04 13:51:52 +0200

    1 commit - creating basic entities and relations.

---
+++ SpringTraining/ldz24/src/main/java/pl/sda/ldz24/Ldz24Application.java
@@ -0,0 +1,13 @@
+package pl.sda.ldz24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ldz24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ldz24Application.class, args);
+    }
+
+}
