9c15e01da4214161f06e2cc0ccbc6777d8c1a4b7
Przemek <wxm@example.com>
2020-08-15 19:40:27 +0200

    www

---
+++ SpringTraining/final24.1/mytestproject/src/main/java/pl/sda/mytestproject/mytestproject/User.java
@@ -0,0 +1,36 @@
+package pl.sda.mytestproject.mytestproject;
+
+public class User {
+
+    String firstName;
+    String lastName;
+    int age;
+
+    public User() {
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
